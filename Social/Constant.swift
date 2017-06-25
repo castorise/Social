7e7eb41e7b7cc79647aa0e9a228d73207f3a9f2d
Chao Zheng <Castor@Chaos-MBP.fios-router.home>
2017-06-25 19:56:42 -0400

    Sign in UI custom classes complete

---
+++ Social/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Social
+//
+//  Created by Chao Zheng on 6/25/17.
+//  Copyright © 2017 Castorise. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
